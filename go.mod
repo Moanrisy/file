2e62d3b78486f363f9de28d625f0986ce38017ed
Moanrisy <moanrisy@gmail.com>
2023-01-14 10:39:47 +0700

    [FEATURE] Created read file function

---
+++ go.mod
@@ -0,0 +1,3 @@
+module git.enigmacamp.com/enigma-camp/enigmacamp-2.0/batch-2/mohammad-anton-rizki-syahputra/handson/go-file
+
+go 1.19
---
+++ read_file.go
@@ -0,0 +1,23 @@
+package go-file
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func ReadByLine(fileName string) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Tidak dapat membuka file")
+	}
+
+	defer file.Close()
+
+	fileScan := bufio.NewScanner(file)
+	fileScan.Split(bufio.ScanLines)
+
+	for fileScan.Scan() {
+		fmt.Println(fileScan.Text())
+	}
+}
